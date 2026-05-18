54952c8b654a6dbba8756ec635624c2950617e8d
Stephen Hellicar <shellicar@gmail.com>
2026-05-18 10:54:43 +1000

    Fix for spaces in cwd

--- .config/tmux/pane-name.sh
+++ .config/tmux/pane-name.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 child=$(pgrep -P "$1" | head -1)
 if [ -n "$child" ]; then
-  ps -o comm= -p "$child"
+  comm=$(ps -o comm= -p "$child")
 else
-  ps -o comm= -p "$1"
+  comm=$(ps -o comm= -p "$1")
 fi
+echo "${comm##*/}"
