05bfb5b18a3a39a4effdeb25ab89b1d32f5d1403
Stephen Hellicar <shellicar@gmail.com>
2026-06-15 02:28:02 +1000

    Resolve Node through fnm instead of nvm

--- os/macos.rc.sh
+++ os/macos.rc.sh
@@ -4,6 +4,4 @@
 # ls -> GNU coreutils (gls); plain ls is already GNU on Linux
 alias ls='gls --color=auto -l'
 
-# nvm (Homebrew)
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # nvm bash_completion
+command -v fnm >/dev/null && eval "$(fnm env --use-on-cd)"
